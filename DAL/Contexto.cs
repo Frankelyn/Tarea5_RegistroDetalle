a7e1f2c4619f8bd9f0c5f832c631ff59a29e2298
Frankelyn <frankelyn_garcia@ucne.edu.do>
2021-10-14 21:02:37 -0400

    Este es el registro de la tarea 5 de aplicada

---
+++ DAL/Contexto.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDetalle.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Permisos> Permisos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 1,
+                Nombre = "Administrador",
+                Descripcion = "Permiso para ser administrador del sistema"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 2,
+                Nombre = "Usuario",
+                Descripcion = "Permiso para entrar como usuario al sistema"
+            });
+
+            modelBuilder.Entity<Permisos>().HasData(new Permisos
+            {
+                PermisoId = 3,
+                Nombre = "Invitado",
+                Descripcion = "Permiso para entrar como invitado del sistema"
+            });
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Roles.db");
+        }
+    }
+}
